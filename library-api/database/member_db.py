0d4b691d99d71f45d4b288ac2091fb4472d2443c
Mendel <MendelCohen770@gmail.com>
2026-06-14 09:56:03 +0300

    add classes signatures

---
+++ library-api/database/member_db.py
@@ -0,0 +1,42 @@
+
+
+class MemberDb:
+    def create_member(self, data):
+        pass
+
+
+    def some(self):
+        pass
+
+
+    def get_all_members(self):
+        pass
+
+
+    def get_member_by_id(self, id):
+        pass
+
+
+    def update_member(self, id, data):
+        pass
+
+
+    def deactivate_member(self, id):
+        pass
+
+
+    def activate_member(self, id):
+        pass
+
+
+    def increment_borrows(self, id):
+        pass
+
+
+    def count_active_members(self):
+        pass
+
+
+    def get_top_member(self):
+        pass
+
